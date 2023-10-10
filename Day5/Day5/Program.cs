84513a16b62e45bb5ee22fdcee2ca647a175c40e
ALBER7K <albertkulawik@gmail.com>
2023-10-10 15:31:30 +0200

    petle

--- Day5/Day5/Program.cs
+++ Day5/Day5/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] cyfry = new int[10];
+cyfry[0] = 0;
+cyfry[1] = 1;
+cyfry[2] = 2;
+cyfry[3] = 3;
+cyfry[4] = 4;
+cyfry[5] = 5;
+cyfry[6] = 6;
+cyfry[7] = 7;
+cyfry[8] = 8;
+cyfry[9] = 9;
+
+Console.WriteLine(cyfry[3]);
+
+for(int i = 0; i < 99; i++)
+{
+    Console.WriteLine(i);
+}
