89d4c486fec5e71f9b5d84eacd8dea50cc4e2c3b
ALBER7K <albertkulawik@gmail.com>
2023-10-10 15:04:21 +0200

    Zadanie 1 dzien 4

--- ChallengeApp21/ChallengeApp21/Program.cs
+++ ChallengeApp21/ChallengeApp21/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string myName = "Ewa";
+int myAge = 33;
+string myGender = "kobieta";
+
+if (myAge < 30 && myGender == "kobieta")
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (myName == "Ewa" && myAge == 33 && myGender == "kobieta")
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (myAge < 18 && myGender == "mężczyzna")
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else
+{
+    Console.WriteLine("Dane nie pokrywają się z treścią zadania");
+}
